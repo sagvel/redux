e3ce419b52edd340141d7a87ce50c2ed160d370c
sagvel <saulko.oleksander@gmail.com>
2022-08-09 09:28:20 +0300

    add class component

--- 01_context/02_user_menu/src/App.jsx
+++ 01_context/02_user_menu/src/App.jsx
@@ -1,17 +1,32 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import { user } from './user_context';
 import { UserContext } from './user_context';
 
-const App = () => {
-  const [userData, setUser] = useState(user);
-  return (
-    <div>
-      <UserContext.Provider value={userData}>
-        <Header />
-      </UserContext.Provider>
-    </div>
-  );
-};
+export default class App extends Component {
+  state = {
+    userData: user,
+  };
+  render() {
+    return (
+      <div>
+        <UserContext.Provider value={this.state.userData}>
+          <Header />
+        </UserContext.Provider>
+      </div>
+    );
+  }
+}
 
-export default App;
+// const App = () => {
+//   const [userData, setUser] = useState(user);
+//   return (
+//     <div>
+//       <UserContext.Provider value={userData}>
+//         <Header />
+//       </UserContext.Provider>
+//     </div>
+//   );
+// };
+
+// export default App;
