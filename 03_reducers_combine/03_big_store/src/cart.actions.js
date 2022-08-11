2169db88c7b687485778210b7d64a46663ba2321
sagvel <saulko.oleksander@gmail.com>
2022-08-11 15:40:28 +0300

    add 3.3 solution

---
+++ 03_reducers_combine/03_big_store/src/cart.actions.js
@@ -0,0 +1,20 @@
+export const ADD_PRODUCT = 'PRODUCT/ADD_PRODUCT';
+export const REMOVE_PRODUCT = 'PRODUCT/REMOVE_PRODUCT';
+
+export const addProduct = productData => {
+  return {
+    type: ADD_PRODUCT,
+    payload: {
+      productData,
+    },
+  };
+};
+
+export const removeProduct = productId => {
+  return {
+    type: REMOVE_PRODUCT,
+    payload: {
+      productId,
+    },
+  };
+};
---
+++ 03_reducers_combine/03_big_store/src/cart.reducer.js
@@ -0,0 +1,21 @@
+import { ADD_PRODUCT, REMOVE_PRODUCT } from './cart.actions';
+
+const initialState = {
+  products: [],
+};
+export const cartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_PRODUCT:
+      return {
+        ...state,
+        products: state.products.concat(action.payload.productData),
+      };
+    case REMOVE_PRODUCT:
+      return {
+        ...state,
+        products: state.products.filter(product => product.id !== action.payload.productId),
+      };
+    default:
+      return state;
+  }
+};
