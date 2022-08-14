372233a0095dd6ab5f3f5394c1b6f280b4b03ca7
sagvel <saulko.oleksander@gmail.com>
2022-08-14 20:45:29 +0300

    add 3.1 solution

---
+++ 03_react_with_redux/01_react_redux_counter/src/Counter.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as counterActions from './counter.actions';
+
+const Counter = ({ counter, increment, decrement, reset }) => {
+  return (
+    <div className="counter">
+      <button className="counter__button" onClick={decrement}>
+        -
+      </button>
+      <span className="counter__value" onClick={reset}>
+        {counter}
+      </span>
+      <button className="counter__button" onClick={increment}>
+        +
+      </button>
+    </div>
+  );
+};
+
+const mapState = state => {
+  return {
+    counter: state,
+  };
+};
+
+const mapDispatch = {
+  increment: counterActions.increment,
+  decrement: counterActions.decrement,
+  reset: counterActions.reset,
+};
+
+export default connect(mapState, mapDispatch)(Counter);
