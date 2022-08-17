48ad1c88f7486e0cca1e743f95c964f22e725d3a
sagvel <saulko.oleksander@gmail.com>
2022-08-17 20:23:59 +0300

    add 6.1 solution

--- 06_async_actions_midleware/01_loger_middleware/src/store.js
+++ 06_async_actions_midleware/01_loger_middleware/src/store.js
@@ -1,12 +1,21 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { counterReducer } from './counter.reducer';
 
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+  return result;
+};
 const reducer = combineReducers({
   counter: counterReducer,
 });
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
 export default store;
